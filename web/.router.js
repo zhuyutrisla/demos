e0ee87f3f51b9e3d63bbca980c774e6d1fbaaa64
zhuyutrisla <a444531326@gmail.com>
2017-07-25 16:07:20 +0800

    0725

---
+++ web/.router.js
@@ -0,0 +1,17 @@
+function setRouter(app){ 
+ var router = app; 
+
+app.get('/loadMore', function(req, res) {
+
+	var curIdx = req.query.index
+	var len = req.query.length
+	var data = []
+
+	for (var i = 0; i< len; i++){
+		data.push('新闻' + (parseInt(curIdx) + i))
+	}
+	setTimeout(function(){
+		res.send(data)
+	},3000)
+})}
+ module.exports.setRouter = setRouter
